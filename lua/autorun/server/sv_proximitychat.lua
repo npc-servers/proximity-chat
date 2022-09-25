076e137ae6561044584c8b03236622db83c78da0
Redox <mail@redox.to>
2022-09-25 18:59:42 +0200

    Optimise hook

--- lua/autorun/server/sv_proximitychat.lua
+++ lua/autorun/server/sv_proximitychat.lua
@@ -1,11 +1,22 @@
-CreateConVar("sv_maxVoiceAudible",1500,FCVAR_ARCHIVE,"Maximum distance for listener to still be able to hear the talker.")
-CreateConVar("sv_proxChatEnable",1,FCVAR_ARCHIVE,"Enables/disables proximity chat.")
+local maxrangecvar = CreateConVar( "proxchat_maxrange", 1000, FCVAR_ARCHIVE, "Maximum distance for listener to still be able to hear the talker." )
+local enabledcvar = CreateConVar( "proxchat_enabled", 1, FCVAR_ARCHIVE, "Enables/disables proximity chat." )
 
-hook.Add("PlayerCanHearPlayersVoice","Maximum Range",function(listener,talker)
-    if (tonumber( listener:GetPos():Distance( talker:GetPos() ) ) > tonumber( GetConVar("sv_maxVoiceAudible"):GetInt()) and tonumber(GetConVar("sv_proxChatEnable"):GetInt()) == 1) then 
-        return false, false
-    else 
-        return true, true
+local maxrange = maxrangecvar:GetInt() ^ 2
+cvars.AddChangeCallback( "proxchat_maxrange", function( _, _, val )
+    maxrange = tonumber( val ) ^ 2
+end )
+
+local enabled = enabledcvar:GetInt() ^ 2
+cvars.AddChangeCallback( "proxchat_enabled", function( _, _, val )
+    enabled = tobool( val )
+end )
+
+hook.Add( "PlayerCanHearPlayersVoice", "proxchat_canhear", function( listener, talker )
+    if not enabled then return end
+
+    if listener:GetPos():DistToSqr( talker:GetPos() ) > maxrange then
+        return false, true
     end
 
+    return true, true
 end )
